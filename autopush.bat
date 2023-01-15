375ff33925d415e6366657e264f34f4e5410f44e
hanxin <hanxin@6736@qq.com>
2023-01-15 20:47:02 +0800

    bat auto push:2023/01/15,20:47:02

--- autopush.bat
+++ autopush.bat
@@ -1,19 +1,22 @@
 @echo off
 
-:: 获取当前脚本的路径
+:: get path
 cd /d %~dp0
-:: 自动提交
+:: auto push
 git init 
 git add . 
- git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
+ git commit -m "bat auto push:%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push origin master
-@echo 已经完成,
+@echo complete,
 
-SET daoTime=60
+SET daoTime=5
 :dao
 set /a daoTime=daoTime-1
 ping -n 2 -w 500 127.1>nul
 cls
-echo 上传Git完成 ,倒计时退出: %daoTime%秒
+echo push complete , quit after %daoTime%s
 if %daoTime%==0 (exit) else (goto dao)
+
+
+
